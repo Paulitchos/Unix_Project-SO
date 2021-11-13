1ccfbd08c7945186b176c59863cc7ff737edf06e
Yeshey <41551785+Yeshey@users.noreply.github.com>
2021-11-13 15:12:51 +0000

    adding template cliente

---
+++ src/cliente.c
@@ -0,0 +1,14 @@
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+int main(int argc, char **argv){
+    printf("Ola eu sou o cliente\n");
+    printf("My PID is: %d\n", getpid());
+    printf("Numero de Argumentos: %d\nArgumentos:\n", argc);
+    for (int i = 0; i < argc; i++){
+        printf(" %s ", argv[i]);
+    }
+    return 0;
+}
